73a6569e6a368b21c5969f37e4617e68e38d27c9
Theodoros Balasis <theodorosbalasis@gmail.com>
2019-05-01 15:23:28 +0300

    Fix segmentation fault due to not checking file pointer, and file creation failure due to invalid filename generation.

--- garbage-dump.c
+++ garbage-dump.c
@@ -4,8 +4,13 @@
 #include <string.h>
 #include <time.h>
 
+// Note: ((rand() % (max + 1 - min)) + min) is used to generate random
+//       characters within a certain range.
+
 // Generates a random string of set size.
 char* randomString(size_t size);
+// Generates a random file name consisting of uppercase characters.
+char* randomFilename(size_t size);
 
 int main(int argc, char* argv[]) {
 	if (argc < 4) {
@@ -37,32 +42,58 @@ int main(int argc, char* argv[]) {
 
 			for (int i = 0; i < numberOfFiles; i++) {
 				// Construct the full file path.
-				filename = randomString(20);
+				filename = randomFilename(20);
 				strcpy(currentFile, targetLocation);
 				strcat(currentFile, "/");
 				strcat(currentFile, filename);
 
 				// Write a random string to the current target path.
 				file = fopen(currentFile, fileMode);
-				char* chars = randomString(chunkSize);
-				fputs(chars, file);
-				fclose(file);
-				free(chars);
+
+				if (file) {
+					char* chars = randomString(chunkSize);
+					fputs(chars, file);
+					fclose(file);
+					free(chars);
+				}
+
+				free(filename);
 			}
+
+			return 0;
 		} else {
 			fprintf(stderr, "Invalid chunk size!");
 			free(targetLocation);
+			return 1;
 		}
 	}
+
+	return 0;
 }
 
 char* randomString(size_t size) {
 	char* chars = malloc(sizeof(char) * size);
 
 	if (chars) {
-		for (int i = 0; i < size; i++) {
-			chars[i] = (rand() % 255) + 1;
+		for (int i = 0; i < (size - 1); i++) {
+			chars[i] = (rand() % 255 + 1 - 1) + 1;
 		}
+
+		chars[size - 1] = 0;
+	}
+
+	return chars;
+}
+
+char* randomFilename(size_t size) {
+	char* chars = malloc(sizeof(char) * size);
+
+	if (chars) {
+		for (int i = 0; i < (size - 1); i++) {
+			chars[i] = (rand() % (90 + 1 - 65)) + 65;
+		}
+
+		chars[size - 1] = 0;
 	}
 
 	return chars;
