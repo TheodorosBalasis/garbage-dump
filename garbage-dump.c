1b1826aedbe077e147749e924c0472502cf0071d
Theodoros <mrkagours@gmail.com>
2019-09-08 20:56:48 +0300

    Change code styling to snake case.

--- garbage-dump.c
+++ garbage-dump.c
@@ -20,79 +20,79 @@ const size_t FILENAME_LENGTH = 20;
 const char FILENAME_LEAST_CHAR = 65;
 const char FILENAME_GREATEST_CHAR = 90;
 
-void generateRandomString(char* buffer, size_t buffer_size, char lower_bound, char upper_bound);
-int directoryExists(char* path);
+void generate_random_string(char* buffer, size_t buffer_size, char lower_bound, char upper_bound);
+int directory_exists(char* path);
 
 int main(int argc, char* argv[]) {
-	int exitCode = 0;
+	int exit_code = 0;
 
 	// Error handling.
 	if (argc != 4) {
 		fprintf(stderr,
 		        "Usage: garbage-dump <target location> <bytes to fill> "
 		        "<file size>\n");
-		exitCode = 1;
-	} else if (!directoryExists(argv[1])){
+		exit_code = 1;
+	} else if (!directory_exists(argv[1])){
 		fprintf(stderr, "Directory not found.\n");
-		exitCode = 2;
-	} else if (directoryExists(argv[1]) == -1) {
+		exit_code = 2;
+	} else if (directory_exists(argv[1]) == -1) {
 		fprintf(stderr, "Directory could not be accessed.\n");
-		exitCode = 3;
+		exit_code = 3;
 	} else {
 		// Program parameters.
-		char* targetLocation = argv[1];
-		size_t capacity = 0, chunkSize = 1, numberOfFiles;
+		char* target_location = argv[1];
+		size_t capacity = 0, chunk_size = 1, number_of_files;
 
 		sscanf(argv[2], "%zu", &capacity);
-		sscanf(argv[3], "%zu", &chunkSize);
-		numberOfFiles = capacity / chunkSize;
+		sscanf(argv[3], "%zu", &chunk_size);
+		number_of_files = capacity / chunk_size;
 
 		if (capacity) {
-			if (chunkSize) {
+			if (chunk_size) {
 				// Initialize the random number generator.
-				time_t currentTime;
-				time(&currentTime);
-				srand(currentTime);
+				time_t current_time;
+				time(&current_time);
+				srand(current_time);
 
-				char fileMode[] = "w";
+				char file_mode[] = "w";
 
 				FILE* file;
 
-				char currentFile[PATH_MAX];
+				char current_file[PATH_MAX];
 				char* filename = malloc(sizeof(char) * FILENAME_LENGTH);
-				char* chars = malloc(sizeof(char) * chunkSize);
+				char* chars = malloc(sizeof(char) * chunk_size);
 
 				// Ensure filename and chars were allocated before continuing.
 				if (filename && chars) {
-					for (int i = 0; i < numberOfFiles; i++) {
+					for (int i = 0; i < number_of_files; i++) {
 						// Construct the full file path.
-						generateRandomString(filename, FILENAME_LENGTH, FILENAME_LEAST_CHAR, FILENAME_GREATEST_CHAR);
-						strncpy(currentFile, targetLocation,
+						generate_random_string(filename, FILENAME_LENGTH, FILENAME_LEAST_CHAR, FILENAME_GREATEST_CHAR);
+						strncpy(current_file, target_location,
 								PATH_MAX - 1 - FILENAME_LENGTH);
-						strcat(currentFile, "/");
-						strncat(currentFile, filename, FILENAME_LENGTH);
+						strcat(current_file, "/");
+						strncat(current_file, filename, FILENAME_LENGTH);
 
 						// Re-generate the filename until one
 						// is generated that doesn't already exist.
-						while (access(currentFile, F_OK) != -1) {
-							generateRandomString(filename, FILENAME_LENGTH, FILENAME_LEAST_CHAR, FILENAME_GREATEST_CHAR);
-							strncpy(currentFile, targetLocation,
+						while (access(current_file, F_OK) != -1) {
+							generate_random_string(filename, FILENAME_LENGTH, FILENAME_LEAST_CHAR, FILENAME_GREATEST_CHAR);
+							strncpy(current_file, target_location,
 									PATH_MAX - 1 - FILENAME_LENGTH);
-							strcat(currentFile, "/");
-							strncat(currentFile, filename, FILENAME_LENGTH);
+							strcat(current_file, "/");
+							strncat(current_file, filename, FILENAME_LENGTH);
 						}
 
 						// Open current target file for writing.
-						file = fopen(currentFile, fileMode);
+						file = fopen(current_file, file_mode);
 
 						if (file) {
-							generateRandomString(chars, chunkSize, 1, 255);
+							generate_random_string(chars, chunk_size, 1, 255);
 							fputs(chars, file);
 							fclose(file);
 						} else {
 							fprintf(stderr,
 							        "Could not open file %s for writing",
-							        currentFile);
+							        current_file);
 						}
 					}
 
@@ -102,20 +102,20 @@ int main(int argc, char* argv[]) {
 					fprintf(stderr,
 					        "Could not allocate buffers for filename and file "
 					        "data.");
-					exitCode = 1;
+					exit_code = 1;
 				}
 			} else {
 				fprintf(stderr, "Invalid chunk size!");
-				free(targetLocation);
-				exitCode = 1;
+				free(target_location);
+				exit_code = 1;
 			}
 		}
 	}
 
-	return exitCode;
+	return exit_code;
 }
 
-void generateRandomString(char* buffer, size_t buffer_size, char lower_bound, char upper_bound) {
+void generate_random_string(char* buffer, size_t buffer_size, char lower_bound, char upper_bound) {
 	if (buffer) {
 		for (int i = 0; i < (buffer_size - 1); ++i) {
 			buffer[i] = (rand() % (upper_bound + 1 - lower_bound)) + lower_bound;
@@ -125,7 +125,7 @@ void generateRandomString(char* buffer, size_t buffer_size, char lower_bound, ch
 	}
 }
 
-int directoryExists(char* path) {
+int directory_exists(char* path) {
 	int exists = 1;
 	DIR* directory = opendir(path);
 	
